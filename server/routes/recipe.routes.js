c4ca0985669d6b25997a02dfcd6795a73dc1b82f
Ayush-Dhawan <ayushdhawan102004@gmail.com>
2024-03-22 15:23:44 +0530

    reading all recipes from backend

--- server/routes/recipe.routes.js
+++ server/routes/recipe.routes.js
@@ -1,10 +1,13 @@
 import express from "express";  
 import protectRoute from "../middleware/protectedRoutes.js";
 import addRecipe from "./recipeControllers/addRecipe.js";
+import readRecipes from "./recipeControllers/readRecipes.js";
+
 
 const router = express.Router();
 
 router.post('/add',protectRoute, addRecipe);
+router.get('/', protectRoute, readRecipes)
 
 
 
---
+++ server/routes/recipeControllers/readRecipes.js
@@ -0,0 +1,17 @@
+import Recipe from "../../models/recipeBlog.model.js";
+
+export default async function readRecipes(req, res) {
+    try {
+        const recipes = await Recipe.find({}); // Fetch all recipes from the database
+        res.status(200).json(recipes); // Send the fetched recipes as a JSON response
+    } catch (error) {
+        console.log("Error in recipe read controller: ", error.message);
+        res.status(500).json({ error: "Internal server error" });
+    }
+}
+
+
+
+// export default async function hello(req, res){
+//     res.status(200).json("hello")
+// }
